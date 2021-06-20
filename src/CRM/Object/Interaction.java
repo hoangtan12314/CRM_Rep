05f6eb41dc1d281dc1db177e008a36be95878835
s3806867 <s3806867@rmit.edu.vn>
2021-06-20 17:35:17 +0700

    Initialize Interaction.java and Lead.java. Adding data to csv files

--- src/CRM/Object/Interaction.java
+++ src/CRM/Object/Interaction.java
@@ -1,4 +1,59 @@
 package CRM.Object;
 
+import java.util.Date;
+
 public class Interaction {
+    private String id;
+    private Date interDate;
+    private String lead;
+    private String mean;
+    private String potential;
+
+    public Interaction(String id, Date interDate, String lead, String mean, String potential) {
+        this.id = id;
+        this.interDate = interDate;
+        this.lead = lead;
+        this.mean = mean;
+        this.potential = potential;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getInterDate() {
+        return interDate;
+    }
+
+    public void setInterDate(Date interDate) {
+        this.interDate = interDate;
+    }
+
+    public String getLead() {
+        return lead;
+    }
+
+    public void setLead(String lead) {
+        this.lead = lead;
+    }
+
+    public String getMean() {
+        return mean;
+    }
+
+    public void setMean(String mean) {
+        this.mean = mean;
+    }
+
+    public String getPotential() {
+        return potential;
+    }
+
+    public void setPotential(String potential) {
+        this.potential = potential;
+    }
 }
--- src/CRM/Object/Lead.java
+++ src/CRM/Object/Lead.java
@@ -1,4 +1,79 @@
 package CRM.Object;
 
+import java.util.Date;
+
 public class Lead {
+    private String id;
+    private String name;
+    private Date dob;
+    private String gender;
+    private String phone;
+    private String email;
+    private String address;
+
+    public Lead(String id, String name, Date dob, String gender, String phone, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
